59f5dd954a22106627ebcc9b2431a0f416f49e12
yehudit lanel <yehuditlanel@gmail.com>
2020-01-08 11:43:23 +0200

    add detail of track to common

---
+++ WebApplication/WebApplication/Controllers/TravelController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    public class TravelController : ApiController
+    {
+        // GET: api/Travel
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Travel/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Travel
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Travel/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Travel/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ common/common/DetailsOfTrack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    public class DetailsOfTrack
+    {
+        public DetailsOfTrack(int trackCode,int travelCode,int driverId,string licensePlate,DateTime dateOfTravel,DateTime hourOfBegin)
+        {
+            this.trackCode = trackCode;
+            this.travelCode = travelCode;
+            this.driverId = driverId;
+            this.licensePlate = licensePlate;
+            this.dateOfTravel = dateOfTravel;
+            this.hourOfBegin = hourOfBegin;
+        }
+        private int trackCode;
+
+        public int TrackCode
+        {
+            get { return trackCode; }
+            set { trackCode = value; }
+        }
+        private int travelCode;
+
+        public int TravelCode
+        {
+            get { return travelCode; }
+            set { travelCode = value; }
+        }
+        private int driverId;
+
+        public int DriverId
+        {
+            get { return driverId; }
+            set { driverId = value; }
+        }
+        private string licensePlate;
+
+        public string LicensePlate
+        {
+            get { return licensePlate; }
+            set { licensePlate = value; }
+        }
+        private DateTime dateOfTravel;
+
+        public DateTime DateOfTravel
+        {
+            get { return dateOfTravel; }
+            set { dateOfTravel = value; }
+        }
+        private DateTime hourOfBegin;
+
+        public DateTime HourOfBegin
+        {
+            get { return hourOfBegin; }
+            set { hourOfBegin = value; }
+        }            
+    }
+}
